a177f82eccc95d9e4b34780e8dc4c24d497b2215
Stephane <stephanegiraolinhares@gmail.com>
2022-10-21 09:15:56 -0300

    teste da camada model

--- 7SongTests/ModelTests/_SongTests.swift
+++ 7SongTests/ModelTests/_SongTests.swift
@@ -10,12 +10,14 @@ import XCTest
 
 final class SongTests: XCTestCase {
 
-//    func test_trackModelInit_dataModelArray() {
-//        let emptyArray: [DataModel] = []
-//        let trackStruct = TracksModel(data: emptyArray)
-//        XCTAssertEqual(trackStruct.data.count, emptyArray.count)
-//    }
+    // Essa função testa o init do TracksModel
+    func test_tracksModelInit_dataModelArray() {
+        let emptyArray: [DataModel] = []
+        let trackStruct = TracksModel(data: emptyArray)
 
+        XCTAssertEqual(trackStruct.data, emptyArray)
+    }
+    // Essa função testa o init do DataModel
     func test_dataModelInit_validDataModel() {
         let title: String = "Love of My Life"
         let id: Int = 1234
@@ -30,19 +32,22 @@ final class SongTests: XCTestCase {
         XCTAssertEqual(validDataModel.id, id)
         XCTAssertEqual(validDataModel.link, link)
     }
+
+    // Essa função testa o init do AlbumModel
     func test_albumModelInit_validAlbumModel() {
         let id: Int = 4321
         let title: String = "Queen"
         let emptyArray: [DataModel] = []
         let tracks: TracksModel = TracksModel(data: emptyArray)
-        let validAlbumModel = AlbumModel(
+
+        let sut = AlbumModel(
             id: id,
             title: title,
             tracks: tracks
         )
-        XCTAssertEqual(validAlbumModel.id, id)
-        XCTAssertEqual(validAlbumModel.title, title)
-        XCTAssertEqual(validAlbumModel.tracks.data.count, tracks.data.count)
-    }
 
+        XCTAssertEqual(sut.id, id)
+        XCTAssertEqual(sut.title, title)
+        XCTAssertEqual(sut.tracks, tracks)
+    }
 }
