14ebdd2927466eca016b4a0899d8cb2c4b29e3eb
Stephane <stephanegiraolinhares@gmail.com>
2022-10-23 13:41:10 -0300

    criando arquivo em documents pelo file manager

--- 7Song/Source/Controller/ViewController.swift
+++ 7Song/Source/Controller/ViewController.swift
@@ -8,38 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    // Criando uma contante para receber a imagem background 
-    private let backgroundImage: UIImageView = {
-        let background = UIImageView(frame: .zero)
-        background.translatesAutoresizingMaskIntoConstraints = false
-        background.image = UIImage(named: "image1")
-        background.contentMode = .scaleAspectFill
-
-        return background
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         APICall.GET()
-    }
-    
-    func setupView() {
-//        view.backgroundColor = .systemBackground
-//        navigationItem.hidesBackButton = true
-    }
-    
-    func setupHierarchy() {
-        view.addSubview(backgroundImage)
-    }
-    
-    func setupConstraints() {
-        NSLayoutConstraint.activate([
-            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
-            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
+        
+        let manager = FileManager.default
+        
+        let url = manager.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        ).first
+        
+        let newFolderUrl = url?.appendingPathComponent("FileManagerFolder")
+        do {
+            try manager.createDirectory(
+                at: newFolderUrl!,
+                withIntermediateDirectories: true,
+                attributes: [:]
+            )
+        } catch {
+            print(error)
+        }
     }
 }
