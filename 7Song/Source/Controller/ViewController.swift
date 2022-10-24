3196a99c9fe47b09ca218648be530f1ccc6990e8
Stephane <stephanegiraolinhares@gmail.com>
2022-10-24 08:11:56 -0300

    primeira parte da persistência

--- 7Song/Source/Controller/ViewController.swift
+++ 7Song/Source/Controller/ViewController.swift
@@ -15,11 +15,13 @@ class ViewController: UIViewController {
         
         let manager = FileManager.default
         
+        // Criando um directory no Documents do meu computador
         let url = manager.urls(
             for: .documentDirectory,
             in: .userDomainMask
         ).first
         
+        // Criando uma pasta em Documents com o nome FileManagerFolder
         let newFolderUrl = url?.appendingPathComponent("FileManagerFolder")
         do {
             try manager.createDirectory(
